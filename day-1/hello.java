91c4e4300eae65f347268449db7eeca55760ca67
Suraj More <lucky.78548more@gmail.com>
2026-02-18 18:35:35 +0530

    day 1

---
+++ day-1/hello.java
@@ -0,0 +1,36 @@
+//a pakage in java is used to group related classes,
+//Think of it as a folder in which you can store related files. It helps to organize your code and avoid naming conflicts.  
+
+public class hello {
+ 
+    public static void main(String[] args) {
+        System.out.println("90 days of code");
+    }
+}
+
+//Naming Conventions in Java:
+//1. Class Names: Class names should be in PascalCase, which means the first letter of
+//   each word in the class name is capitalized. For example, MyClass, MyFirstClass.
+
+//for functions and variables, the naming convention is camelCase, which means the first letter of the first word is lowercase,
+// and the first letter of each subsequent word is capitalized. For example, myFunction, myVariable.
+
+//PSVM is a default function in java which is the entry point of any java program. It stands for "public static void main". When you run a java program, the JVM (Java Virtual Machine) looks for this function to start executing the code.
+// public is an Access Modifiers
+// static is an keyword which helps us while coding to use the code without creating an object of a class.
+
+// Basic Structure of the Java Prohgram:ṇ
+// 1. Function: The main function is the entry point of the program where the execution starts.
+// 2. Class: A class is a blueprint for creating objects. It defines the properties and behaviors of the objects.
+
+// Realtime example
+// Class is HOUSEHOLD REPAIR
+// Function is REPAIRING A LEAKY TAP
+// function is PLumber
+// function is Carpenter
+// Function is Electrician
+// function is Painter
+
+// class is an entity where we are stroring all the functions or all teh functions are been accessed from the class.
+
+// void defines that after execution of code its not give the default value as zero.( 0 )
